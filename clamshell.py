b848c15108f3eab35f941fe6793e4f40a7ce2bb6
Conor T. Ryan <ctryan3@gmail.com>
2014-05-20 08:26:59 -0700

    Improve error handling

--- clamshell.py
+++ clamshell.py
@@ -5,20 +5,43 @@ import sys
 
 class Clamshell():
 
-    def __init__(self):
+    bindings = {}
+
+    def __init__(self, bindings):
+        self.bindings = bindings
         clamshellrl.set_dispatch_callback(self.dispatch)
 
     def dispatch(self, expr):
         if expr == '':
             return
-        if expr == 'exit':
-            sys.exit()
         cmd, argv = parser.parse(expr)
-        print cmd, argv
+        if cmd in self.bindings.keys():
+            fn = self.bindings[cmd]
+            arg_count = fn.func_code.co_argcount
+            if len(argv) != arg_count:
+                msg = "Error: '{}' takes {} argument{}, not {}"
+                plural = ''
+                if arg_count > 1: plural = 's'
+                print msg.format(cmd, arg_count, plural, len(argv))
+                return
+            fn.__call__(*argv)
+        else:
+            print "Error: unknown command: '{}'".format(cmd)
+
+    def bind(self, cmd, fn):
+        self.bindings[cmd] = fn
 
     def start(self):
         clamshellrl.start()
 
 if __name__ == '__main__':
-    shell = Clamshell()
+
+    def say_hello(name):
+        print "Hello {}!".format(name)
+
+    shell = Clamshell({
+        'exit': sys.exit,
+        'say_hello': say_hello
+    })
+
     shell.start()
